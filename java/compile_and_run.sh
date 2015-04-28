0d486e90e0046753f13d365edf21b68b05b2d390
Samuel Albrecht <elsamuko@gmail.com>
2015-04-28 07:27:57 +0200

    chg: more flexible compile script

--- java/compile_and_run.sh
+++ java/compile_and_run.sh
@@ -1,8 +1,21 @@
 #!/usr/bin/env bash
 
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $(basename "$0") <file>"
+    exit 1
+fi
+
+if [ ! -f "$1" ]; then
+    echo "Could not find $1"
+    exit 1
+fi
+
+SOURCE="$1"
+CLASS="${1%.java}"
+
 echo "Compiling..."
-javac HelloWorld.java
+javac "$SOURCE"
 
 echo "Starting..."
-java HelloWorld
+java "$CLASS"
 
