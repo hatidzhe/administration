8ce1eeaa3c529c45fe1e90023cef085f29fd5c4c
Hatidzhe <yusufova.h@gmail.com>
2015-05-01 16:54:46 +0300

    Endversion

--- java/Counter.java
+++ java/Counter.java
@@ -1,6 +1,5 @@
 import java.io.*;
 import java.util.regex.*;
-
 class Counter
 {
 public static void main(String args[]) throws Exception
@@ -11,10 +10,17 @@ String str=null;
 int count=0;
 while((str=br.readLine())!=null)
 {
-Pattern pat=Pattern.compile("[ ]");
-String str1[]=pat.split(str);
-count=count+str1.length;
+    Pattern pat=Pattern.compile("[ ]");
+    String str1[]=pat.split(str);
+    
+     // count = count + str1.length;
+    for( String s     : str1 ) {
+        if( ! s.isEmpty() ) {
+            count++;
+        }
+    }
 }
 System.out.println("the number of the words in the file are "+count);
 }
 }
+
