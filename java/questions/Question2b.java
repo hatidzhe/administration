b6b5ee7dbf2c713ac4ac47697b33760466289874
Hatidzhe <yusufova.h@gmail.com>
2015-05-04 12:28:25 +0300

    The relational operators

---
+++ java/questions/Question2b.java
@@ -0,0 +1,19 @@
+public class Question2b{
+ public static void main(String[] args) {
+  int i = 10, j = 5;
+  boolean r, r1, r2, r3, r4, r5;
+  r = i == j;
+System.out.println(r);
+  r1 = i != j;
+System.out.println(r1);
+  r2 = i > j;
+System.out.println(r2);
+  r3 = i >= j;
+System.out.println(r3);
+  r4 = i < j;
+System.out.println(r4);
+  r5 = i <= j;
+System.out.println(r5);
+}
+}  
+
