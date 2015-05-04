e4c47fe2a0ed346d989e74c04433b6768ea18721
Hatidzhe <yusufova.h@gmail.com>
2015-05-04 15:26:47 +0300

    Assignment operators

---
+++ java/questions/Question2d.java
@@ -0,0 +1,10 @@
+public class Question2d {
+  public static void main(String[] args){
+     int i = 10,j = 5;
+         i += j;
+     int A = 19,C = 10;
+         A *= C;
+     System.out.println(i);
+     System.out.println(A);
+}
+}
