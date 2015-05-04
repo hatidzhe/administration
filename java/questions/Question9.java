78bab02a3dba0037f0ea9c03d4cd12207519747c
Hatidzhe <yusufova.h@gmail.com>
2015-05-04 20:59:27 +0300

    If/else-condition

---
+++ java/questions/Question9.java
@@ -0,0 +1,16 @@
+public class Question9 
+                                      {
+                                        public static void main(String[] args)
+                                        {
+                                          System.out.println("Zahlen");
+                                          int v1;
+                                          v1 = Integer.valueOf(args[0]).intValue();
+                                          
+                                          if(v1<0)
+                                            System.out.println("Die Zahl ist negativ.");
+                                          else
+                                            System.out.println("Die Zahl ist positiv.");
+                                            System.exit(0);
+                                        }
+                                      }	
+                                      
