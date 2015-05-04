ad4e9d884700da59b80d04b8ca9c149ce84e335a
Hatidzhe <yusufova.h@gmail.com>
2015-05-04 12:04:26 +0300

    The aritmetic operators

---
+++ java/questions/Question2.java
@@ -0,0 +1,21 @@
+public class Question2 {
+ public static void main(String[] args) {
+   int i = 10, j = 5, r;
+   int k = 7, n = 6, l;
+   int  q = 15, g = 60, u;
+   int  a = 4, b = 8, c;
+   int    p = 6, h = 9, z;
+
+       r = i + j;
+       l = k - n;
+       u = q * g;
+       c = b / a;
+       z = h % p;
+  System.out.println(r);
+  System.out.println(l);
+  System.out.println(u);
+  System.out.println(c);
+  System.out.println(z);
+}
+}
+
