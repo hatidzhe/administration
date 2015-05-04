d0a6fad1e815c6430776fb418606ad4b3c9b3fdc
Hatidzhe <yusufova.h@gmail.com>
2015-05-04 14:22:36 +0300

    Locical operators

---
+++ java/questions/Question2c.java
@@ -0,0 +1,14 @@
+public class Question2c {
+  public static void main(String[] args) {
+  boolean i = true, j = false;
+  boolean r1, r2, r3, r4;
+  r1 = i && j;
+  r2 = i || j;
+  r3 = i ^ j;
+  r4 = !i;
+  System.out.println(r1);
+  System.out.println(r2);
+  System.out.println(r3);
+  System.out.println(r4);
+}
+}
