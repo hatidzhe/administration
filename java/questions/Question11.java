44e700171ef8a7d77c5f2eec18c93c45e108b054
Hatidzhe <yusufova.h@gmail.com>
2015-05-05 10:17:00 +0300

    Example for a boolean logic

---
+++ java/questions/Question11.java
@@ -0,0 +1,13 @@
+public class Question11 {
+ public static void main(String[] args) {
+  // these are boolean variables     
+      boolean A = true;
+      boolean B = false; 
+
+      System.out.println("A|B = "+(A|B));
+      System.out.println("A&B = "+(A&B));
+      System.out.println("!A = "+(!A));
+      System.out.println("A^B = "+(A^B));
+      System.out.println("(A|B)&A = "+((A|B)&A));
+    }
+}
