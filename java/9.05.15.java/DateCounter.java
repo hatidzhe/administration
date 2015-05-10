9eac3f4337d115fc6d6c9c2b639cd0d9d74ec452
Samuel Albrecht <elsamuko@gmail.com>
2015-05-10 14:38:42 +0200

    add: DateCounter.java

--- java/9.05.15.java/DateCounter.java
+++ java/9.05.15.java/DateCounter.java
@@ -1,6 +1,17 @@
 import java.util.*;
 
 public class DateCounter {
+
+    static long diffInDays( Calendar first, Calendar second ) {
+        long days = 0;
+        return 0;
+    }
+
+    static long diffFromToday( Calendar cal ) {
+        long days = 0;
+        return 0;
+    }
+
     public static void main( String[] args ) {
         Calendar Anfangsdatum = Calendar.getInstance();
         Calendar Enddatum = Calendar.getInstance();
